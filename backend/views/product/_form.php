a668998ec7bf14c6cb37147cfd2748416d4a0211
Nurlytan <nurik.bro.2000@mail.ru>
2022-08-17 11:44:33 +0600

    Product Creating Form Integration

--- backend/views/product/_form.php
+++ backend/views/product/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
+use dosamigos\ckeditor\CKEditor;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Product */
@@ -10,17 +11,48 @@ use yii\bootstrap4\ActiveForm;
 
 <div class="product-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin([
+        'enableClientValidation' => true,
+    ]); ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'description')->widget(CKEditor::className(), [
+        'options' => ['rows'=>6],
+        'preset'=>'basic'
+    ])
+    ?>
 
-    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+    <!-- <div class="input-group mb-3">
+    <div class="input-group-prepend">
+        <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+    </div>
+    <div class="custom-file">
+        <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+    </div>
+    </div> -->
 
-    <?= $form->field($model, 'price')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'image', [
+        'template'=>'
+                <div class="custom-file">
+                    {input}
+                    {label}
+                    {error}
+                </div>
+        ',
+        'labelOptions' => [
+            'class' => 'custom-file-label',
+            'label' => 'Choose Image For Product'
+        ],
+        'inputOptions' => [
+            'class' => 'custom-file-input'
+        ]
+    ])->textInput(['type'=>'file']) ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?= $form->field($model, 'price')->textInput(['maxlength' => true, 'type'=>'number']) ?>
+
+    <?= $form->field($model, 'status')->checkbox() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
